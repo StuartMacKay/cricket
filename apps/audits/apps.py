1753c67c145b0eae5ac1ca55ccad29603f317308
Stuart MacKay <smackay@fastmail.com>
2026-06-07 10:26:11 +0100

    Enable Write-ahead Logging (WAL) for SQLite

--- apps/audits/apps.py
+++ apps/audits/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class Config(AppConfig):
     name = "audits"
+
+    def ready(self):
+        from django.db.backends.signals import connection_created
+
+        def enable_wal(sender, connection, **kwargs):
+            if connection.vendor == "sqlite":
+                connection.cursor().execute("PRAGMA journal_mode=WAL;")
+
+        connection_created.connect(enable_wal)
